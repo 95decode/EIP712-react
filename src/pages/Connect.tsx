71db6a80171f1ddf39b3c3485ed117f7b5a405a3
95decode <95decode@gmail.com>
2022-12-19 12:38:10 +0900

    Add basic order cases (buy, sell)

--- src/pages/Connect.tsx
+++ src/pages/Connect.tsx
@@ -1,3 +1,5 @@
+// TODO : refactoring
+
 import { useWeb3React } from '@web3-react/core';
 import { ethers } from "ethers";
 import ABI from "abi/abi.json";
@@ -191,107 +193,43 @@ function Connect() {
             verifyingContract: '0x413Ae7516A0eEbebF4c33061c5f908F1CF8C8Ce5'
         };
 
-    const types = {
-        EIP712Domain: [
-            { name: 'name', type: 'string' },
-            { name: 'version', type: 'string' },
-            { name: 'chainId', type: 'uint256' },
-            { name: 'verifyingContract', type: 'address' },
-        ],
-        OrderComponents: [
-            { name: "offerer", type: "address" },
-            { name: "zone", type: "address" },
-            { name: "offer", type: "OfferItem[]" },
-            { name: "consideration", type: "ConsiderationItem[]" },
-            { name: "orderType", type: "uint8" },
-            { name: "startTime", type: "uint256" },
-            { name: "endTime", type: "uint256" },
-            { name: "zoneHash", type: "bytes32" },
-            { name: "salt", type: "uint256" },
-            { name: "conduitKey", type: "bytes32" },
-            { name: "counter", type: "uint256" },
-        ],
-        OfferItem: [
-            { name: "itemType", type: "uint8" },
-            { name: "token", type: "address" },
-            { name: "identifierOrCriteria", type: "uint256" },
-            { name: "startAmount", type: "uint256" },
-            { name: "endAmount", type: "uint256" },
-        ],
-        ConsiderationItem: [
-            { name: "itemType", type: "uint8" },
-            { name: "token", type: "address" },
-            { name: "identifierOrCriteria", type: "uint256" },
-            { name: "startAmount", type: "uint256" },
-            { name: "endAmount", type: "uint256" },
-            { name: "recipient", type: "address" },
-        ],
-    };
-        
-/*
-
-// 판매 서명
-
-offerer:
-0x00000000879Cd60de9fEaC82452cead7a07E18df
-offer:
-    0:
-        itemType:
-            2
-        token:
-            0xD0E0f719C72Ff82957957890435687F2444ef28f
-        identifierOrCriteria:
-            1
-        startAmount:
-            1
-        endAmount:
-            1
-consideration:
-0:
-    itemType:
-        0
-    token:
-        0x0000000000000000000000000000000000000000
-    identifierOrCriteria:
-        0
-    startAmount:
-        48750000000000000
-    endAmount:
-        48750000000000000
-    recipient:
-        0x00000000879Cd60de9fEaC82452cead7a07E18df
-1:
-    itemType:
-        0
-    token:
-        0x0000000000000000000000000000000000000000
-    identifierOrCriteria:
-        0
-    startAmount:
-        1250000000000000
-    endAmount:
-        250000000000000
-    recipient:
-        0x0000a26b00c1F0DF003000390027140000fAa719
-startTime:
-1671154331
-endTime:
-1673832731
-orderType:
-0
-zone:
-0x0000000000000000000000000000000000000000
-zoneHash:
-0x0000000000000000000000000000000000000000000000000000000000000000
-salt:
-24446860302761739304752683030156737591518664810215442929807093780685310352753
-conduitKey:
-0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000
-counter:
-0
-
-*/
-
+        const types = {
+            EIP712Domain: [
+                { name: 'name', type: 'string' },
+                { name: 'version', type: 'string' },
+                { name: 'chainId', type: 'uint256' },
+                { name: 'verifyingContract', type: 'address' },
+            ],
+            OrderComponents: [
+                { name: "offerer", type: "address" },
+                { name: "zone", type: "address" },
+                { name: "offer", type: "OfferItem[]" },
+                { name: "consideration", type: "ConsiderationItem[]" },
+                { name: "orderType", type: "uint8" },
+                { name: "startTime", type: "uint256" },
+                { name: "endTime", type: "uint256" },
+                { name: "zoneHash", type: "bytes32" },
+                { name: "salt", type: "uint256" },
+                { name: "conduitKey", type: "bytes32" },
+                { name: "counter", type: "uint256" },
+            ],
+            OfferItem: [
+                { name: "itemType", type: "uint8" },
+                { name: "token", type: "address" },
+                { name: "identifierOrCriteria", type: "uint256" },
+                { name: "startAmount", type: "uint256" },
+                { name: "endAmount", type: "uint256" },
+            ],
+            ConsiderationItem: [
+                { name: "itemType", type: "uint8" },
+                { name: "token", type: "address" },
+                { name: "identifierOrCriteria", type: "uint256" },
+                { name: "startAmount", type: "uint256" },
+                { name: "endAmount", type: "uint256" },
+                { name: "recipient", type: "address" },
+            ],
+        };
+            
         const message = {
             offerer: "0x00000000879Cd60de9fEaC82452cead7a07E18df",
             zone: "0x0000000000000000000000000000000000000000",
@@ -387,6 +325,7 @@ struct AdditionalRecipient {
 
 */
 
+            //consideration 서명 내용 포함
             let basicOrderParameters = {
                 considerationToken: "0x0000000000000000000000000000000000000000",
                 considerationIdentifier: 0,
@@ -429,6 +368,210 @@ struct AdditionalRecipient {
         }
     }
 
+    const consideration2 = async () => {
+        const domain = {
+            name: 'Seaport',
+            version: '1.1',
+            chainId: 5,
+            verifyingContract: '0x413Ae7516A0eEbebF4c33061c5f908F1CF8C8Ce5'
+        };
+
+        const types = {
+            EIP712Domain: [
+                { name: 'name', type: 'string' },
+                { name: 'version', type: 'string' },
+                { name: 'chainId', type: 'uint256' },
+                { name: 'verifyingContract', type: 'address' },
+            ],
+            OrderComponents: [
+                { name: "offerer", type: "address" },
+                { name: "zone", type: "address" },
+                { name: "offer", type: "OfferItem[]" },
+                { name: "consideration", type: "ConsiderationItem[]" },
+                { name: "orderType", type: "uint8" },
+                { name: "startTime", type: "uint256" },
+                { name: "endTime", type: "uint256" },
+                { name: "zoneHash", type: "bytes32" },
+                { name: "salt", type: "uint256" },
+                { name: "conduitKey", type: "bytes32" },
+                { name: "counter", type: "uint256" },
+            ],
+            OfferItem: [
+                { name: "itemType", type: "uint8" },
+                { name: "token", type: "address" },
+                { name: "identifierOrCriteria", type: "uint256" },
+                { name: "startAmount", type: "uint256" },
+                { name: "endAmount", type: "uint256" },
+            ],
+            ConsiderationItem: [
+                { name: "itemType", type: "uint8" },
+                { name: "token", type: "address" },
+                { name: "identifierOrCriteria", type: "uint256" },
+                { name: "startAmount", type: "uint256" },
+                { name: "endAmount", type: "uint256" },
+                { name: "recipient", type: "address" },
+            ],
+        };
+            
+        const message = {
+            offerer: "0x00000000879Cd60de9fEaC82452cead7a07E18df",
+            zone: "0x0000000000000000000000000000000000000000",
+            offer:[
+                {
+                    itemType: 2,
+                    token: "0xc3D1Ecb231bBAe23A978aDBf3119E826E7Eb3828",
+                    identifierOrCriteria: 0,
+                    startAmount: 1,
+                    endAmount: 1
+                }
+            ],
+            consideration:[
+                {
+                    itemType: 0,
+                    token: "0x0000000000000000000000000000000000000000",
+                    identifierOrCriteria: 0,
+                    startAmount: "48750000000000000",
+                    endAmount: "48750000000000000",
+                    recipient: "0x00000000879Cd60de9fEaC82452cead7a07E18df"
+                },{
+                    itemType: 0,
+                    token: "0x0000000000000000000000000000000000000000",
+                    identifierOrCriteria: 0,
+                    startAmount: "1250000000000000",
+                    endAmount: "1250000000000000",
+                    recipient: "0x69f5b75D9e20921e07F7B3AD0751BB1C06D915CA"
+                }
+            ],
+            orderType: 0,
+            startTime: 1,
+            endTime: 100000000000000,
+            zoneHash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+            salt: "0x0000000012345600000000000000000000000000000000000000000000000000",
+            conduitKey: "0x00000000879Cd60de9fEaC82452cead7a07E18df000000000000000000000000",
+            counter: 0
+        }
+
+        const msgParams = JSON.stringify({
+            domain: domain,
+            message: message,
+            primaryType: 'OrderComponents',
+            types: types
+        })
+
+        const params = [account, msgParams];
+
+        console.log(await library.provider.request({
+            method: "eth_signTypedData_v4",
+            params: params,
+            from: account
+        }));
+
+        // result : 0x0c4d8487295548f9bc5493c6cb0aaace2174f87612c8389daddd129a0b775bd040d79c3d535e79ce4ba657f607fb233f0b055ea0c51a7a4048e2d0c3bfc2f1211c
+    }
+
+    const sell = async () => {
+        try {
+            let seaport = "0x413Ae7516A0eEbebF4c33061c5f908F1CF8C8Ce5";
+            let seaportABI = seaportAbi;
+
+            let contract = new ethers.Contract(seaport, seaportABI, library.getSigner());
+
+/*
+
+offerer:
+    0x00000000879Cd60de9fEaC82452cead7a07E18df
+offer:
+    0:
+        itemType:
+            1
+        token:
+            0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
+        identifierOrCriteria:
+            0  
+        startAmount:
+            1000000000000000
+        endAmount:
+            1000000000000000
+consideration:
+    0:
+        itemType:
+            2
+        token:
+            0x15987A0417D14cc6f3554166bCB4A590f6891B18
+        identifierOrCriteria:
+            213576
+        startAmount:
+            1
+        endAmount:
+            1
+        recipient:
+            0x00000000879Cd60de9fEaC82452cead7a07E18df
+    1:
+        itemType:
+            1
+        token:
+            0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
+        identifierOrCriteria:
+            0
+        startAmount:
+            25000000000000
+        endAmount:
+            25000000000000
+        recipient:
+            0x0000a26b00c1F0DF003000390027140000fAa719
+    2:
+        itemType:
+            1
+        token:
+            0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
+        identifierOrCriteria:
+            0
+        startAmount:
+            50000000000000
+        endAmount:
+            50000000000000
+        recipient:
+            0x3c8D9f130970358b7E8cbc1DbD0a1EbA6EBE368F
+startTime:
+    1671420347
+endTime:
+    1671679543
+orderType:
+    0    
+zone:
+    0x0000000000000000000000000000000000000000
+zoneHash:
+    0x0000000000000000000000000000000000000000000000000000000000000000
+salt:
+    24446860302761739304752683030156737591518664810215442929804057035552702189998
+conduitKey:
+    0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000
+counter:
+    0
+
+*/
+
+            // offer 서명 내용 포함
+            let basicOrderParameters = {
+
+            };
+
+            // MUST update channel to true
+
+            const tx = await contract.fulfillBasicOrder(basicOrderParameters, {
+                value: "50000000000000000",
+                gasLimit: 173292
+            });
+
+            const receipt = await tx.wait();
+            console.log(receipt);
+
+            return receipt.transactionHash;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div>
             <div>
@@ -456,50 +599,17 @@ struct AdditionalRecipient {
             </div>
             <div>
                 <button type="button" onClick={consideration}>sign</button>
+                <button type="button" onClick={consideration2}>sign2</button>
             </div>
             <div>
-                <p>buy</p>
+                <p>transact</p>
             </div>
             <div>
-                <button type="button" onClick={buy}>transact</button>
+                <button type="button" onClick={buy}>buy</button>
+                <button type="button" onClick={sell}>sell</button>
             </div>
         </div>
     );
 }
 
 export default Connect;
-
-/*
-
-const orderType4 = {
-  OrderComponents: [
-    { name: "offerer", type: "address" },
-    { name: "zone", type: "address" },
-    { name: "offer", type: "OfferItem[]" },
-    { name: "consideration", type: "ConsiderationItem[]" },
-    { name: "orderType", type: "uint8" },
-    { name: "startTime", type: "uint256" },
-    { name: "endTime", type: "uint256" },
-    { name: "zoneHash", type: "bytes32" },
-    { name: "salt", type: "uint256" },
-    { name: "conduitKey", type: "bytes32" },
-    { name: "counter", type: "uint256" },
-  ],
-  OfferItem: [
-    { name: "itemType", type: "uint8" },
-    { name: "token", type: "address" },
-    { name: "identifierOrCriteria", type: "uint256" },
-    { name: "startAmount", type: "uint256" },
-    { name: "endAmount", type: "uint256" },
-  ],
-  ConsiderationItem: [
-    { name: "itemType", type: "uint8" },
-    { name: "token", type: "address" },
-    { name: "identifierOrCriteria", type: "uint256" },
-    { name: "startAmount", type: "uint256" },
-    { name: "endAmount", type: "uint256" },
-    { name: "recipient", type: "address" },
-  ],
-};
-
-*/
